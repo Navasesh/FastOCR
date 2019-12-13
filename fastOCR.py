9a4cccb14dcd7942fd6b1da33ff68d606a2e0255
Jack YANG <jingkangy91@gmail.com>
2019-12-13 20:29:56 +0800

    Rename image size and delete comments.

--- fastOCR.py
+++ fastOCR.py
@@ -22,9 +22,9 @@ for old_img in os.listdir(old_dir):
     try:
         img = Image.open(old_name)
         # print(img.size)
-        img = img.resize((1500,800))
-        # print(img.size)
-        img_crop = img.crop(box_size)
+        img_resize = img.resize((1500, 800))
+        # print(img_resize.size)
+        img_crop = img_resize.crop(box_size)
         img_crop.save(os.path.join(tmp_dir, old_img.split('.')[0] + '_tmp.png'))
         text = pytesseract.image_to_string(img_crop).strip()
         reg = re.compile('\d{10}')
@@ -47,13 +47,3 @@ for old_img in os.listdir(old_dir):
         pass
 
 send2trash.send2trash(tmp_dir)
-
-# fapiao = Image.open('img/fapiao.jpg')
-
-# fapiao1 = fapiao.crop(box_size)
-
-# fapiao1.save("img/fapiao1.jpg")
-
-# text = pytesseract.image_to_string(fapiao1).strip()
-
-# print(text)
