77c5bdffb5ccbf2b388fffb72e0b3e1c66b16c4c
Jack YANG <jingkangy91@gmail.com>
2019-12-13 18:33:05 +0800

    Add try catch and comments.

--- fastOCR.py
+++ fastOCR.py
@@ -7,24 +7,34 @@ import pytesseract
 old_dir = 'img_old'
 new_dir = 'img_new'
 
-print(os.path.exists(old_dir))
+if os.path.exists(old_dir) is not True:
+    print('The "img_old" dir does not exist, please check.')
+
 os.makedirs(new_dir, exist_ok=True)
 
 box_size = (280, 50, 500, 100)
 
 for old_img in os.listdir(old_dir):
     old_name = os.path.join(old_dir, old_img)
-    img = Image.open(old_name)
-    img_crop = img.crop(box_size)
-    text = pytesseract.image_to_string(img_crop).strip()
-    new_img = text + '.png'
+    try:
+        img = Image.open(old_name)
+        img_crop = img.crop(box_size)
+        text = pytesseract.image_to_string(img_crop).strip()
+        new_img = text + '.png'
 
-    new_name = os.path.join(new_dir, new_img)
+        new_name = os.path.join(new_dir, new_img)
 
-    print('Renaming "%s" to "%s"...' % (old_name, new_name))
+        print('----------' +
+              'Renaming "%s" to "%s"...' % (old_name, new_name) +
+              '----------')
 
-    # shutil.move(old_name, new_name)
-    shutil.copy(old_name, new_name)
+        # shutil.move(old_name, new_name)
+        shutil.copy(old_name, new_name)
+    except IOError:
+        print('The filetype of "%s" is not image.' % old_name)
+    else:
+        pass
+
 
 # fapiao = Image.open('img/fapiao.jpg')
 
